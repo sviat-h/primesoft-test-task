8acb0f256345bcae961e15767640dc749e8e4a12
sviat-h <svyatoslav.grinik@gmail.com>
2022-04-26 17:08:09 +0300

    created init_db.sql file

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,36 @@
+CREATE SCHEMA IF NOT EXISTS `primesoft` DEFAULT CHARACTER SET utf8;
+USE `primesoft`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nick` varchar(32) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login_UNIQUE` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE IF NOT EXISTS `vehicles` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `login` varchar(32) NOT NULL,
+  `brand` varchar(32) NOT NULL,
+  `model` varchar(32) NOT NULL,
+  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `login_UNIQUE` (`login`),
+  CONSTRAINT `vehicles_users_fk` FOREIGN KEY (`login`) REFERENCES `users` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE IF NOT EXISTS `insurance_offers` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `vehicle_id` bigint NOT NULL,
+  `insurer` varchar(32) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `vehicle_id_UNIQUE` (`vehicle_id`),
+  CONSTRAINT `insurance_offers_vehicles_fk` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
